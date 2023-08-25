e8f3ddd2b615f4f1e3a1b9b1582d20ad594cc050
Johannes Tuikkala <johannes@vaadin.com>
2023-08-25 11:31:32 +0300

    solution

--- src/main/java/com/vaadin/training/clientside/stepper/ServersidestepperUI.java
+++ src/main/java/com/vaadin/training/clientside/stepper/ServersidestepperUI.java
@@ -1,20 +1,32 @@
 package com.vaadin.training.clientside.stepper;
 
+import javax.servlet.annotation.WebServlet;
+
+import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.server.VaadinRequest;
+import com.vaadin.server.VaadinServlet;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 
+@SuppressWarnings("serial")
 public class ServersidestepperUI extends UI {
-	@Override
-	public void init(VaadinRequest request) {
-		Label label = new Label("Hello Vaadin user");
-		VerticalLayout layout = new VerticalLayout();
-		//TODO Stepper stepper = new Stepper();
-		layout.addComponent(new Label("Stepper component"));
-		//TODO layout.addComponent(stepper);
-		layout.addComponent(label);
-		//TODO label.setPropertyDataSource(stepper);
-		setContent(layout);
-	}
+
+    @WebServlet(value = "/*", asyncSupported = true)
+    @VaadinServletConfiguration(productionMode = false, ui = ServersidestepperUI.class, widgetset = "com.vaadin.training.clientside.stepper.WidgetSet")
+    public static class Servlet extends VaadinServlet {
+    }
+
+    @Override
+    public void init(VaadinRequest request) {
+        Label label = new Label("Hello Vaadin user");
+        VerticalLayout layout = new VerticalLayout();
+
+        Stepper stepper = new Stepper();
+        layout.addComponent(stepper);
+        layout.addComponent(label);
+        label.setPropertyDataSource(stepper);
+        setContent(layout);
+    }
+
 }
